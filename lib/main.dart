5441ab29cdd5cdbab768f67a23e6a14e986376c2
NickWu <wujunao19950715@gmail.com>
2018-08-09 12:05:01 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Nudge',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Flutter Nudge'),
+      ),
+      body: Container(),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Add new item',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+}
